d6146df7d8c05cb0e00352df7f05d24622f06ab8
CRIFFAD <Criffad24@gmail.com>
2024-06-02 19:34:31 +0100

    assignment

--- level 2/level2.js
+++ level 2/level2.js
@@ -1,5 +1,4 @@
 // 1.Using console.log() print out the following statement: The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.
-// console.log(challenge.repeat(There is no exercise better for the heart than reaching down and lifting people up.));
 //2.Using console.log() print out the following quote by Mother Teresa:"Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."
 // 3.Check if typeof '10' is exactly equal to 10. If not make it exactly equal.
 // 4.Check if parseFloat('9.8') is equal to 10 if not make it exactly equal with 10.
@@ -17,6 +16,8 @@
 // 4 1 4 16 64
 // 5 1 5 25 125
 // 12.Use substr to slice out the phrase because because because from the following sentence:'You cannot end a sentence with because because because is a conjunction'
+console.log("There is no exercise better for the heart than reaching down and lifting people up.");
+console.log("Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.");
 console.log(typeof '10' === 10);
    console.log(parseInt('10') === 10);
 
